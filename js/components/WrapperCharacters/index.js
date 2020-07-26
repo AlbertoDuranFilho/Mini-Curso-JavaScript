fac5577d3863a3557f2ddb5552409f10fc9bc287
AlbertoDuranFilho <67593467+AlbertoDuranFilho@users.noreply.github.com>
2020-07-26 08:45:44 -0300

    Create index.js

---
+++ js/components/WrapperCharacters/index.js
@@ -0,0 +1,18 @@
+const pathGueio = "images/gueio.png"
+
+const gueio = Character `
+    width: 18%;
+    ${pathGueio}
+`
+const WrapperCharacters = (css, children) => (
+    `
+    <div style="${css}">${children}</div>
+    `
+)
+
+const wrapperCharacters = WrapperCharacters`
+    display: flex;
+    justify-content: space-around;
+    ${gueio + gueio + gueio}
+`
+
