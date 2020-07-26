0e967fd0325e15cc9c8b87fd3e55ce8ce8949b6b
AlbertoDuranFilho <67593467+AlbertoDuranFilho@users.noreply.github.com>
2020-07-26 18:21:14 -0300

    Update index.js

--- js/components/Slide/index.js
+++ js/components/Slide/index.js
@@ -1,32 +1,123 @@
-const Action = () => (
+const states = [true, false, false]
+
+const Action = (css) => {
+
+    createStyle(css)
     
-)
+    return(
+        `<li class="action"></li>`
+        )
+}
 
-const Item = (css, content = '') => (
-    `<li style="${css}"> ${content}</li>`
-
-)
+const Item = (css, className) => {
+    createStyle(css)
+    
+    return(`
+        <li class="item ${className}"
+        onclick="handClick(event)"></li>
+    `)
+}
 
 const Slide = (css, content) => (
     `<ul style="${css}">${content}</ul>`
 )
 
-const item = Item`
+const action = Action`
+    .action{
     list-style: none;
-    height: calc(var(--line-height) * 3);
-    width: calc(var(--line-height )* 3);
-    background-color: var(--happy-color);
+    background-color: var(--action-color);
+    height: calc(var(--line-height)*4);
+    width: calc(var(--line-height)*4);
+    position: absolute;
+    left:-5px;
+    transition: transform 300ms linear;
+    }
+
+    .action.second{
+        transform: translatex(293px) rotate(360deg);
+    }
+
+    .action.third{
+        transform: translatex(585px) rotate(720deg);
+    }
 `
 
-const slideContent = 'Vai'
+
+
 const slide = Slide`
     display: flex;
     align-items: center;
     justify-content: space-between;
+    position: relative;
     width: 80%;
     height: var(--line-height);
     background-color: var(--happy-color);
     margin-top: auto;
-    ${item + item + item}
+    ${showItems(states) + action}
     
 `
+
+function showItems(statesItems){
+    
+    const item = (pos) => Item`
+        .item{
+        list-style: none;
+        height: calc(var(--line-height)*3);
+        width: calc(var(--line-height )*3);
+        background-color: var(--happy-color);
+        transition: transform 100ms linear;
+        cursor: pointer;
+    }
+    ${`pos-${pos}`}
+`
+
+    const itemActive = (pos) => Item`
+    .item.active{
+        transform: scale(1.6);
+    }
+
+    ${`pos-${pos}`}
+    `
+
+    const items = statesItems .map((state, index) => {
+       if(state){
+           return itemActive(index + 1)
+       } 
+       return item(index +1)
+    })
+
+    return items.join('')
+}
+
+function clearAction(action){
+    action.classList.remove('second')
+    action.classList.remove('third')
+}
+
+function handClick(event){
+    const {target} = event
+    const allItems = document.querySelectorAll('.item')
+    const action = document.querySelector('.action')
+
+    allItems.forEach(item => item.classList.remove('active') )
+    target.classList.add('active');
+
+    clearAction(action)
+
+    if(target.classList.contains('pos-2')){
+        action.classList.add('second')
+    }
+    if(target.classList.contains('pos-3')){
+        action.classList.add('third')
+    }
+}
+
+function createStyle(css){
+    const head = document.querySelector("head")
+    const style = `
+        <style>
+            ${css}
+        </style>
+    `
+    head.insertAdjacentHTML("beforeend", style)
+}
